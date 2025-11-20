4b83ffe267495003294483256493f64dc065eb70
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2025-11-20 01:02:33 +0100

    Add soft pause feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { DebatePhaseBadge } from '@/app/DebatePhaseBadge'
 import { DebatePhase } from '@/app/debatePhase'
+import { Badge } from '@/components/ui/badge'
 import { IconButton } from '@/components/ui/shadcn-io/icon-button'
-import { PauseIcon, PlayIcon } from 'lucide-react'
-import { useEffect } from 'react'
+import { ClockIcon, PauseIcon, PlayIcon, SquareIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { useStopwatch } from 'react-timer-hook'
 
 const computePhase = (passedSeconds: number): DebatePhase => {
@@ -24,7 +25,29 @@ const computePhase = (passedSeconds: number): DebatePhase => {
   return 'ended'
 }
 
+const getIconForButton = ({
+  isSoftPaused,
+  isRunning,
+  minutes,
+}: {
+  isSoftPaused: boolean
+  isRunning: boolean
+  minutes: number
+}) => {
+  if (!isRunning) {
+    return PlayIcon
+  }
+  if (isSoftPaused) {
+    return SquareIcon
+  }
+  if (minutes > 0) {
+    return SquareIcon
+  }
+  return PauseIcon
+}
+
 export default function Home() {
+  const [isSoftPaused, setIsSoftPaused] = useState(false)
   const {
     start: startTimer,
     pause: pauseTimer,
@@ -46,23 +69,44 @@ export default function Home() {
     <main className="grid min-h-screen w-full grid-cols-1 grid-rows-[1fr,auto,1fr] justify-items-center gap-8 p-8">
       <div className=""></div>
       <div className="grid place-content-center justify-items-center gap-8">
-        <p className="text-7xl font-bold">
-          {minutes.toString().padStart(2, '0')}:
-          {seconds.toString().padStart(2, '0')}
-        </p>
-        <DebatePhaseBadge currentPhase={currentPhase} />
+        {isSoftPaused ? (
+          <>
+            <p className="font-mono text-7xl font-bold text-slate-500">
+              {(6 - minutes).toString().padStart(2, '0')}:
+              {(59 - seconds).toString().padStart(2, '0')}
+            </p>
+            <Badge className="bg-slate-500 text-3xl text-white [&>svg]:size-6">
+              <ClockIcon />
+              Preparation time
+            </Badge>
+          </>
+        ) : (
+          <>
+            <p className="font-mono text-7xl font-bold">
+              {minutes.toString().padStart(2, '0')}:
+              {seconds.toString().padStart(2, '0')}
+            </p>
+            <DebatePhaseBadge currentPhase={currentPhase} />
+          </>
+        )}
         <IconButton
-          icon={isRunning ? PauseIcon : PlayIcon}
+          icon={getIconForButton({
+            isRunning,
+            isSoftPaused,
+            minutes,
+          })}
           active={isRunning}
           size="xl"
           color={[250, 250, 250]}
           className="bg-slate-900"
           onClick={() => {
-            if (isRunning) {
-              pauseTimer()
+            if (!isRunning) {
+              resetTimer(undefined, true)
+              setIsSoftPaused(false)
+            } else if (minutes < 7 && !isSoftPaused) {
+              setIsSoftPaused(true)
             } else {
-              resetTimer()
-              startTimer()
+              pauseTimer()
             }
           }}
         />
