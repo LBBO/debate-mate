c3aa58bf507be0794d4fe7cbb7f1c2dc799a67ef
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2026-04-26 14:21:13 +0200

    Finish removing the safe zone lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,6 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_swipe_back_ios::init())
-        .plugin(tauri_plugin_safe_area_insets_css::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
